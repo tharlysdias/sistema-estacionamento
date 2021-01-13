2851864864d04a4ed208247a0f466de0d238868f
Tharlys Dias <tharlys.souza@outlook.com>
2021-01-13 15:32:12 -0300

    Movimentos Rotativos - Calculos de valores

--- core/admin.py
+++ core/admin.py
@@ -9,8 +9,14 @@ from .models import (
 
 # Register your models here.
 
+
+class MovRotativoAdmin(admin.ModelAdmin):
+    list_display = (
+        'checkin', 'checkout', 'valor_hora', 'veiculo', 'pago', 'total', 'horas_total')
+
+
 admin.site.register(Marca)
 admin.site.register(Veiculo)
 admin.site.register(Pessoa)
 admin.site.register(Parametros)
-admin.site.register(MovRotativo)
+admin.site.register(MovRotativo, MovRotativoAdmin)
--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import math
 
 # Create your models here.
 
@@ -44,6 +45,12 @@ class MovRotativo(models.Model):
     valor_hora = models.DecimalField(max_digits=5, decimal_places=2)
     veiculo = models.ForeignKey(Veiculo, on_delete=models.CASCADE)
     pago = models.BooleanField(default=False)
+
+    def horas_total(self):
+        return math.ceil((self.checkout - self.checkin).total_seconds() / 3600)
+
+    def total(self):
+        return self.valor_hora * self.horas_total()
 
     def __str__(self):
         return self.veiculo.placa
