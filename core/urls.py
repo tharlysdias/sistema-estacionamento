9ae68cab745a8dbc703665015cdd43f0333b9a16
Tharlys Dias <tharlys.souza@outlook.com>
2021-01-15 12:22:32 -0300

    Listagem Pessoa

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from django.conf.urls import url, include
-from .views import home
+from .views import home, lista_pessoas
 
 urlpatterns = [
-    url(r'^$', home, name='core_home')
+    url(r'^$', home, name='core_home'),
+    url(r'^pessoas/$', lista_pessoas, name='core_lista_pessoas')
 ]
--- core/views.py
+++ core/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
+from .models import Pessoa
 
 # Create your views here.
 
 def home(request):
     context = {'mensagem': 'Olá Mundo!'}
     return render(request, 'core/index.html', context)
+
+
+def lista_pessoas(request):
+    pessoas = Pessoa.objects.all()
+    return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
