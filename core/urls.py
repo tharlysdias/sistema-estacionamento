306f8a49ca823769f6ab52af2557409cb4222f4e
Tharlys Dias <tharlys.souza@outlook.com>
2021-01-15 12:39:11 -0300

    Listagem veiculos e MovRotativo

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
 from django.conf.urls import url, include
-from .views import home, lista_pessoas
+from .views import (
+    home, 
+    lista_pessoas, 
+    lista_veiculos,
+    lista_movrotativos
+    )
 
 urlpatterns = [
     url(r'^$', home, name='core_home'),
-    url(r'^pessoas/$', lista_pessoas, name='core_lista_pessoas')
+    url(r'^pessoas/$', lista_pessoas, name='core_lista_pessoas'),
+    url(r'^veiculos/$', lista_veiculos, name='core_lista_veiculos'),
+    url(r'^mov-rot-list/$', lista_movrotativos, 
+        name='core_lista_movrotativos'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-from .models import Pessoa
+from .models import (
+    Pessoa, 
+    Veiculo,
+    MovRotativo
+)
 
 # Create your views here.
 
@@ -11,3 +15,15 @@ def home(request):
 def lista_pessoas(request):
     pessoas = Pessoa.objects.all()
     return render(request, 'core/lista_pessoas.html', {'pessoas': pessoas})
+
+
+def lista_veiculos(request):
+    veiculos = Veiculo.objects.all()
+    return render(request, 'core/lista_veiculos.html', {'veiculos': veiculos})
+
+
+def lista_movrotativos(request):
+    mov_rot = MovRotativo.objects.all()
+    return render(
+        request, 'core/lista_movrotativos.html', {'mov_rot': mov_rot}
+    )
